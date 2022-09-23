98cd028ebad76cf581e1d775b4db312c43a863f9
Yqnk <yan.bertile@gmail.com>
2022-09-23 14:50:05 +0200

    options added

--- main.c
+++ main.c
@@ -2,28 +2,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+// not yet #include <getopt.h>
 
+
+/*
+ * returns :
+ * - 0 -> success
+ * - 1 -> help provided
+ * - 2 -> error
+*/
 int main(int argc, char *argv[])
 {
-    if (argc > 2)
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
     {
-        printf("Error: to many arguments supplied.");
+        // print help
+        printf("help");
         return 1;
     }
-
-    size_t n = strlen(argv[1]) - 1;
-    char * s = malloc(n + 1);
-
-    if(s == NULL)
+    else if ((strcmp(argv[1], "-t") || strcmp(argv[1], "--target")) && argc == 3)
     {
-        printf("Error: allocation failed");
-    }
+        // exec with argv[2] as the target string
+        size_t n = strlen(argv[2]) - 1;
+        char * s = malloc(n + 1);
 
-    strcpy(s, argv[1]);
-    mix(s, n);
-    printf("%s", s);
-    free(s);
-    return 0;
+        if(s == NULL)
+        {
+            printf("error: allocation failed :(");
+            return 2;
+        }
+
+        strcpy(s, argv[2]);
+        mix(s, n);
+        printf("%s", s);
+        free(s);
+        return 0;
+    }
+    else
+    {
+        printf("error: read (shuffle -h) :)");
+        return 2;
+    }
 }
 
 // int main()
